175d4f5a23e576eaae37c00146427898bb7b0276
c4benni <c4benni@gmail.com>
2022-01-29 09:12:01 +0100

    create default utils

---
+++ src/components/UiTransition/utils/defaults.ts
@@ -0,0 +1,9 @@
+import { ConfigProp } from "../types/props/config";
+
+export default {
+  config: "fade" as ConfigProp,
+  previousTransform: undefined as Boolean | undefined,
+  group: undefined as Boolean | undefined,
+  optimise: true as Boolean | undefined,
+  prefix: "uit-" as string,
+};
