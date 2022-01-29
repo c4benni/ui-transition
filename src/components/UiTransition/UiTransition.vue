2ad12c3b31b65380eeacf0db9550295fb59862cb
c4benni <c4benni@gmail.com>
2022-01-29 10:47:06 +0100

    Update UiTransition.vue

--- src/components/UiTransition/UiTransition.vue
+++ src/components/UiTransition/UiTransition.vue
@@ -3,43 +3,27 @@ import {
   computed,
   defineComponent,
   h,
-  nextTick,
   onBeforeMount,
-  ref,
-  Teleport,
 } from "@vue/runtime-core";
 import { Transition } from "@vue/runtime-dom";
-import props from "./props";
-import { getAnimSavePath, keyframeName } from "./utils/component";
 
-// import addWorkerEvent from './utils/addWorkerEvent'
+import { DynamicObject, GlobalState } from "./types/utils";
+
+import props from "./props";
+
+import { createStyleNode, keyframeName } from "./utils/component";
+
+import eventHooks from "./utils/eventHooks";
+
+import workerBox from './utils/workerBox'
 
 import createWorker from "./utils/createWorker";
 
-import asyncWorker from "./utils/asyncWorker";
-import { DynamicObject } from "./types/utils";
-
-let styleCreated = false;
-
-let styleId = "";
-
-let workerCreated = false;
-
-function createStyleNode(): void {
-  if (!styleCreated) {
-    const styleNode = document.createElement("style");
-
-    styleId = `ui-${performance.now().toString(36).replace(/\./g, "-")}`;
-
-    styleNode.id = styleId;
-
-    (document.head || document.querySelector("head")).append(styleNode);
-
-    styleCreated = true;
-  }
+const globalState: GlobalState = {
+  styleCreated:false,
+  styleId:'',
+  workerCreated:false
 }
-
-let webWorker: Worker;
 
 // store keyframes names as key and a truthy value as value;
 // so looking up and deleting will be faster and easy on mem.
@@ -67,14 +51,12 @@ export default defineComponent({
     const getKeyframeName = computed(() => keyframeName(tempConfig));
 
     onBeforeMount(() => {
-      createStyleNode();
+      createStyleNode(globalState);
 
-      if (!workerCreated) {
-        webWorker = createWorker();
+      if (!globalState.workerCreated) {
+        workerBox.webWorker = createWorker();
 
-        // do this if $uiSpring is enabled;
-
-        workerCreated = true;
+        globalState.workerCreated = true;
       }
     });
 
@@ -82,93 +64,15 @@ export default defineComponent({
       h(
         Transition,
         {
-          onBeforeEnter: async (e) => {
-            const el = e as unknown as HTMLElement;
+          css: false,
+          appear: true,
 
-            el.classList.add("ui-transition");
-
-            const styleProps: DynamicObject<string | number> = {
-              "--uit-delay": tempConfig.delay,
-              "--uit-opacity": tempConfig.from.opacity,
-              "--uit-transform": tempConfig.from.transform,
-            };
-
-            for (const key in styleProps) {
-              el.style.setProperty(
-                key,
-                `${styleProps[key]}`.replace(/\{|\}/g, "")
-              );
-            }
-
-            if (!keyframes[getKeyframeName.value]) {
-              const animObject = await asyncWorker({
-                worker: webWorker,
-                type: "spring",
-                parse: true,
-                data: {
-                  ...tempConfig,
-                  savePath: getAnimSavePath(tempConfig),
-                  keyframeName: getKeyframeName.value,
-                },
-              });
-
-              const cssText = animObject.data.cssText;
-
-              keyframes[getKeyframeName.value] = cssText.duration;
-
-              const styleTag = document.getElementById(`${styleId}`);
-
-              if (styleTag) {
-                styleTag.innerText += animObject.data.cssText.keyframes;
-              }
-            }
-          },
-
-          onEnter: async (e, done) => {
-            await nextTick();
-
-            const el = e as unknown as HTMLElement;
-
-            const eventCallback = (evt: AnimationEvent) => {
-              if (evt.target === evt.currentTarget) {
-                const elem = evt.target as unknown as HTMLElement | null;
-
-                done();
-
-                [
-                  "--uit-anim-duration",
-                  "--uit-anim-name",
-                  "--uit-delay",
-                  "--uit-transform",
-                  "--uit-opacity",
-                ].forEach((prop) => {
-                  elem?.style.removeProperty(prop)
-                });
-
-                elem?.classList.remove('ui-transition');
-
-                elem?.removeEventListener('animationend',eventCallback)
-              }
-            };
-
-            el.addEventListener("animationend", eventCallback);
-
-            const timeout = setTimeout(() => {
-              const styleProps: DynamicObject<string> = {
-                "--uit-anim-duration": `${keyframes[getKeyframeName.value]}ms`,
-                "--uit-anim-name": getKeyframeName.value,
-              };
-
-              console.log(keyframes[getKeyframeName.value]);
-              
-
-              for (const key in styleProps) {
-                el.style.setProperty(key, styleProps[key]);
-              }
-
-              clearTimeout(timeout);
-            }, !keyframes[getKeyframeName.value] ? 16 : 0);
-          },
+          ...eventHooks({
+            tempConfig,
+            keyframes,
+            getKeyframeName,
+            styleId: globalState.styleId,
+          }),
         },
         {
           default: () => slots.default?.() || null,
