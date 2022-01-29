475b43e4895ae815ef9e6bcab9f8aed735ec32f1
c4benni <c4benni@gmail.com>
2022-01-29 21:24:14 +0100

    Update defaults.ts

--- src/components/UiTransition/utils/defaults.ts
+++ src/components/UiTransition/utils/defaults.ts
@@ -1,7 +1,7 @@
 import { ConfigProp } from "../types/props/config";
 
 export default {
-  config: "fade" as ConfigProp,
+  config: "scale(0.8, 1)" as ConfigProp,
   previousTransform: undefined as Boolean | undefined,
   group: undefined as Boolean | undefined,
   optimise: true as Boolean | undefined,
