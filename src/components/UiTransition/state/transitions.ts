b8bb9b854ad9b5889697a738918921e8cf94b741
c4benni <c4benni@gmail.com>
2022-03-04 22:59:39 +0100

    feat: create fade transition

--- src/components/UiTransition/state/transitions.ts
+++ src/components/UiTransition/state/transitions.ts
@@ -1,7 +1,36 @@
 import { reactive } from "vue";
-import { DynamicObject } from "../../UiTransition/types/utils";
-import { BuildAnim } from "../types";
+import { DynamicObject } from "../types";
+import { ConstructAnim } from "./types";
 
-const transitions = reactive<DynamicObject<(args?: []) => BuildAnim>>({});
+let transitions = reactive<DynamicObject<ConstructAnim>>({
+  fade: (from: number = 0, to: number = 1) => {
+    return {
+      frame: (step, phase) => {
+        const build = {
+          enter: {
+            opacity: `${step(0, to)}`,
+            transform: `scale3d(${step(from, to)}, ${step(from, to)}, 1)`,
+          },
+          leave: {
+            opacity: `${step(to, from)}`,
+          },
+        };
+
+        return build[phase];
+      },
+    };
+  },
+});
 
 export default transitions;
+
+export function addTransition(name: string, transition: ConstructAnim) {
+  transitions = {
+    ...transitions,
+    [name]: transition,
+  };
+}
+
+export function removeTransition(name: string) {
+  delete transitions[name];
+}
