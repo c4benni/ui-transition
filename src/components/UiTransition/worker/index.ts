d36dd52c0c94302848359871e60fd874ea1cf86d
c4benni <c4benni@gmail.com>
2022-03-10 23:51:01 +0100

    fix: createSpring function

--- src/components/UiTransition/worker/index.ts
+++ src/components/UiTransition/worker/index.ts
@@ -2,7 +2,6 @@ import { BuildAnim, DynamicObject } from "../types";
 
 declare global {
   interface Window {
-    createSpring?: () => number[];
     saved: DynamicObject<any>;
     transitions: DynamicObject<(...args: any[]) => BuildAnim>;
     methods: DynamicObject<any>;
@@ -81,97 +80,96 @@ const worker = function () {
           const { data } = eventData;
 
           // add the `createSpring` function to the global instance of the worker.
-          !self.createSpring &&
-            (self.createSpring = (): number[] => {
-              const {
-                tension = 170,
-                friction = 26,
-                mass = 1,
-                precision = 0.01,
-                velocity = 0,
-                stopAttempt = 5,
-              } = data.config || {};
+          const createSpring = (): number[] => {
+            const {
+              tension = 170,
+              friction = 26,
+              mass = 1,
+              precision = 0.01,
+              velocity = 0,
+              stopAttempt = 5,
+            } = data.config || {};
 
-              //   path to save this `createSpring` call, to avoid repetition
-              const savePath =
-                `${tension}-${friction}-${mass}-${precision}-${stopAttempt}-${velocity}`.replace(
-                  /\./g,
-                  "-"
-                );
+            //   path to save this `createSpring` call, to avoid repetition
+            const savePath =
+              `${tension}-${friction}-${mass}-${precision}-${stopAttempt}-${velocity}`.replace(
+                /\./g,
+                "-"
+              );
 
-              if (self.saved?.[savePath]) {
-                return self.saved[savePath];
+            if (self.saved?.[savePath]) {
+              return self.saved[savePath];
+            }
+
+            //   get springs from 0 to 1;
+            //   then interpolate it with any other value(s)
+            let current = 0;
+
+            const to = 1;
+
+            //   initial velocity
+            let _velocity = 0;
+
+            //   frames created; on every change in the `current` position, a new frame is created
+            let frames = 0;
+
+            //   `halt` holds how many times the difference between the current spring and previous spring is <= the `precision`.
+            //   This means having a higher precision (closer to 1) will result in a very short spring. While having a lower precision (closer to 0) will result in a smoother, but longer spring.
+            let halt = 0;
+
+            //   `stoppingAttempt` is how many times the spring should halt (check `halt` above), before it comes to a stop. This means if you need the final moments of the spring to go back and forth a few times, use a smaller number (> 0). But if you need a fine tuned spring ending, use a higher `stoppingAttempt`.
+            const stoppingAttempt = Math.max(Math.abs(stopAttempt), 1) || 5;
+
+            //   `positions` holds an array of spring values. Each item in this array is considered a frame.
+            const positions: number[] = [];
+
+            //   to achieve 60fps. This shouldn't be changed. However, reducing the frames to say 1/30 will result in a choppy looking spring (not smooth). While increasing it eg (1/120) will make the spring unnecessarily long with no visible difference from the 1/60 frames.
+            const FPS = 1 / 60;
+
+            //   loop through 1000 times to create at most 1000 frames.
+            for (let step = 0; step <= 1000; step += 1) {
+              const springForce = -tension * (current - to);
+
+              const frictionForce = -friction * _velocity;
+
+              const acceleration = (springForce + frictionForce) / mass;
+
+              _velocity += acceleration * FPS;
+
+              const nextValue = current + _velocity * FPS;
+
+              const stopping =
+                Math.abs(nextValue - current) <= Math.abs(precision);
+
+              if (stopping) {
+                halt += 1;
               }
 
-              //   get springs from 0 to 1;
-              //   then interpolate it with any other value(s)
-              let current = 0;
-
-              const to = 1;
-
-              //   initial velocity
-              let _velocity = 0;
-
-              //   frames created; on every change in the `current` position, a new frame is created
-              let frames = 0;
-
-              //   `halt` holds how many times the difference between the current spring and previous spring is <= the `precision`.
-              //   This means having a higher precision (closer to 1) will result in a very short spring. While having a lower precision (closer to 0) will result in a smoother, but longer spring.
-              let halt = 0;
-
-              //   `stoppingAttempt` is how many times the spring should halt (check `halt` above), before it comes to a stop. This means if you need the final moments of the spring to go back and forth a few times, use a smaller number (> 0). But if you need a fine tuned spring ending, use a higher `stoppingAttempt`.
-              const stoppingAttempt = Math.max(Math.abs(stopAttempt), 1) || 5;
-
-              //   `positions` holds an array of spring values. Each item in this array is considered a frame.
-              const positions: number[] = [];
-
-              //   to achieve 60fps. This shouldn't be changed. However, reducing the frames to say 1/30 will result in a choppy looking spring (not smooth). While increasing it eg (1/120) will make the spring unnecessarily long with no visible difference from the 1/60 frames.
-              const FPS = 1 / 60;
-
-              //   loop through 1000 times to create at most 1000 frames.
-              for (let step = 0; step <= 1000; step += 1) {
-                const springForce = -tension * (current - to);
-
-                const frictionForce = -friction * _velocity;
-
-                const acceleration = (springForce + frictionForce) / mass;
-
-                _velocity += acceleration * FPS;
-
-                const nextValue = current + _velocity * FPS;
-
-                const stopping =
-                  Math.abs(nextValue - current) <= Math.abs(precision);
-
-                if (stopping) {
-                  halt += 1;
-                }
-
-                if (halt >= stoppingAttempt) {
-                  positions.push(to);
-                  frames = step + 1;
-                  break;
-                }
-
-                current = nextValue;
-
-                if (step == 0) {
-                  positions.push(0);
-                }
-
-                positions.push(current);
-              }
-
-              if (frames == 0) {
-                frames = 1000;
+              if (halt >= stoppingAttempt) {
                 positions.push(to);
+                frames = step + 1;
+                break;
               }
 
-              //   save results to avoid repetition
-              return (self.saved[savePath] = positions);
-            });
+              current = nextValue;
 
-          const springValues = self.createSpring();
+              if (step == 0) {
+                positions.push(0);
+              }
+
+              positions.push(current);
+            }
+
+            if (frames == 0) {
+              frames = 1000;
+              positions.push(to);
+            }
+
+            //   save results to avoid repetition
+            return (self.saved[savePath] = positions);
+          };
+
+          const springValues = createSpring();
 
           // create a path to hold the current values the spring will be created from.
           const savePath = `${JSON.stringify(data)}`;
