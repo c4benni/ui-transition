934648d916ed5f92cdab68c16d35ff550265ad09
c4benni <c4benni@gmail.com>
2022-03-12 23:54:00 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,40 @@
+import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import typescript from "@rollup/plugin-typescript";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "UiTransition",
+      fileName: (format) => `ui-transition.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // Add external deps here
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+  plugins: [
+    {
+      ...typescript({ tsconfig: "./tsconfig.json" }),
+      apply: "build",
+    },
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "/src"),
+    },
+  },
   server: {
     port: 9999,
     host: "0.0.0.0",
