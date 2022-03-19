05dd06b8b990ad776fb102c0c2fcf24e1e6b276d
c4benni <c4benni@gmail.com>
2022-03-20 00:13:27 +0100

    feat: memoize injectGetSpring

--- src/getSpring/inject/index.ts
+++ src/getSpring/inject/index.ts
@@ -1,9 +1,18 @@
+import { DynamicObject } from "src/types";
 import extractSpring from "../../utils/extractSpring";
 import calcSpring from "../calcSpring";
 import calculateSteps from "../interpolate/calculateSteps";
 import { InjectGetSpring } from "./type";
 
+const saved: DynamicObject<(number[] | number[][])[]> = {};
+
 const injectGetSpring: InjectGetSpring = async (frame, config) => {
+  const savePath = `${frame}~${config}`;
+
+  if (saved[savePath]) {
+    return saved[savePath];
+  }
+
   if (Array.isArray(frame) && frame.length) {
     const springConfig = config
       ? extractSpring(config, config, "leave")
@@ -38,10 +47,10 @@ const injectGetSpring: InjectGetSpring = async (frame, config) => {
       }
     });
 
-    return output;
+    return (saved[savePath] = output);
   }
 
-  return [];
+  return (saved[savePath] = []);
 };
 
 export default injectGetSpring;
